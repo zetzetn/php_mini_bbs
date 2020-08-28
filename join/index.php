0dd136597a66d689a91b368609b797bbc42decf7
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-28 22:26:45 +0900

    メールアドレス、パスワードエラ〜メッセージ修正

--- join/index.php
+++ join/index.php
@@ -2,6 +2,12 @@
 if ($_POST['name'] === ''){
 		$error['name'] = 'blank';
 }
+if ($_POST['email'] === ''){
+	$error['email'] = 'blank';
+}
+if ($_POST['password'] === ''){
+	$error['password'] = 'blank';
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,18 +32,25 @@ if ($_POST['name'] === ''){
 	<dl>
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'],ENT_QUOTES)); ?>" />
 					<?php if ($error['name'] === 'blank'): ?>
-					<p class="error">* ニックネームを入力してください</p>
+					<p class="error">*ニックネームを入力してください</p>
 					<?php endif; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+        	<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
+					<?php if ($error['email'] === 'blank'): ?>
+					<p class="error">*メールアドレスを入力してください</p>
+					<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
         </dd>
+				<?php if ($error['password'] === 'blank'): ?>
+					<p class="error">*パスワードを入力してください</p>
+					<?php endif; ?>
+
 		<dt>写真など</dt>
 		<dd>
         	<input type="file" name="image" size="35" value="test"  />
