01e141c57759dbb9e7cc8fd2bd3a6a95f17c358f
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-28 22:32:33 +0900

    パスワード4文字以上　メッセージ作成

--- join/index.php
+++ join/index.php
@@ -5,6 +5,9 @@ if ($_POST['name'] === ''){
 if ($_POST['email'] === ''){
 	$error['email'] = 'blank';
 }
+if (strlen($_POST['password']) < 4){
+	$error['password'] = 'length';
+}
 if ($_POST['password'] === ''){
 	$error['password'] = 'blank';
 }
@@ -47,7 +50,10 @@ if ($_POST['password'] === ''){
 		<dd>
         	<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
         </dd>
-				<?php if ($error['password'] === 'blank'): ?>
+				<?php if ($error['password'] === 'length'): ?>
+					<p class="error">*パスワードは4文字以上で入力してください</p>
+					<?php endif; ?>
+					<?php if ($error['password'] === 'blank'): ?>
 					<p class="error">*パスワードを入力してください</p>
 					<?php endif; ?>
 
